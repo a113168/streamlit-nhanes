819284d2778f97121eda3477abce943db0f58d6b
a113168 <a113168@alunos.uminho.pt>
2026-05-13 14:50:01 +0100

    Add files via upload

--- graf_tlc.py
+++ graf_tlc.py
@@ -9,16 +9,16 @@ st.write("Laboratório de Estatísticas II - Projeto de Grupo")
 st.header("Teorema do Limite Central")
 
 # Linha para escolher o número de amostras
-n = st.slider("Tamanho da amostra", 1, 1000, 50)
+n = st.slider("Tamanho da amostra", 1, 1000, 30)
 
 
 # Simulações aleatórias
-simulacoes = 1000
+simulacoes = 10000
 amostras = np.random.uniform(0, 1, size=(simulacoes, n))
 medias_amostras = amostras.mean(axis=1)
 
 # Média e desvio padrão teóricos
-media_teorica= 0.5
+media_teorica = 0.5
 desvio_padrao_teorico = np.sqrt(1 / 12) / np.sqrt(n)
 
 # Criar espaço para o gráfico
@@ -26,14 +26,15 @@ fig, ax = plt.subplots()
 
 # Criar histograma
 ax.hist(medias_amostras,
-        bins = 30, # quantidade de barras
+        bins = 15, # quantidade de barras
         density = True, # comparar com curva teórica
         color = "lightblue", # cor das barras
-        edgecolor = "black") # cor da borda das barras
+        edgecolor = "white") # cor da borda das barras
 
 # Distribuição Normal (teórico) - Linha vermelha
 x = np.linspace(medias_amostras.min(), medias_amostras.max(), 100) # criar valores
 y = norm.pdf(x, media_teorica, desvio_padrao_teorico) # altura da linha
+
 
 # Apresentar linha, Definir título e label's
 ax.plot(x, y, color = "red", linewidth = 2)
